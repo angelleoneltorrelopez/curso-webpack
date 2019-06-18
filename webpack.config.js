08fe6d90685b60daad7f462bd4f70c99c74b7acc
angelleoneltorrelopez <angelleoneltorrelopez@github.com>
2019-06-18 00:27:46 -0600

    comentarios

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/app.js', //archivo principal
   output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js'
+    path: __dirname + '/build', //direccion y nombre de carpeta salida
+    filename: 'bundle.js' // nombre del archivo generado
   },
 
   devServer: {
-    port: 5000
+    port: 5000 //configurar puerto
   },
 
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [{ loader: MiniCssExtractPlugin.loader },
+        use: [{ loader: MiniCssExtractPlugin.loader },//plugin crea un archivo aparte de css
           { loader: 'css-loader' },
         {loader: 'sass-loader'}],
       },
@@ -24,10 +24,10 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({ //plugin add archivo html
       template: './src/index.html'
     }),
-    new MiniCssExtractPlugin({
+    new MiniCssExtractPlugin({ // plubin salida css
       filename: 'bundle.css'
     })
   ]
